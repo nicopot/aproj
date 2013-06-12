44dc0220703743328495026ce7d32d649b3bf7db
nicopot <potin.nico@gmail.com>
2013-06-12 20:56:12 +0200

    conf index

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
 $(document).ready(function() {	
-alert('test');
+
 $('body').on('touchmove', function(evt) { evt.preventDefault(); })
 
-$('#page_start').swipe({swipeLeft:function(event, direction, distance, duration, fingerCount) {$(this).css({"-webkit-transform":"translateX(-1024px)"});$('#page_menu').show();$('#page_menu').css({"-webkit-transform":"translateX(-1024px)"});},threshold:0});
-$('#page_menu').swipe({swipeRight:function(event, direction, distance, duration, fingerCount) {$(this).css({"-webkit-transform":"translateX(+1024px)"});$('#page_start').css({"-webkit-transform":"translateX(0px)"});},threshold:0});
+$('#page_start').swipe({swipeLeft:function(event, direction, distance, duration, fingerCount) {$(this).transition({x:'-1024px'});$('#page_menu').show();$('#page_menu').transition({x:'-1024px'});},threshold:0});
+$('#page_menu').swipe({swipeRight:function(event, direction, distance, duration, fingerCount) {$(this).transition({x:'1024px'});;$('#page_start').transition({x:'0px'});},threshold:0});
 
-$('#page_menu').on('transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd', 
+$('#page_menu').on('webkitTransitionEnd',
+function(event) {
+ $('#bloc_menu_1').css({'margin-left':'1024px'});  
+$('#bloc_menu_1').on('webkitTransitionEnd', 
 function() {
- 
+ $('#bloc_menu_2').css({'margin-left':'1024px'});  
+});
+$('#bloc_menu_2').on('webkitTransitionEnd', 
+function() {
+ $('#bloc_menu_3').css({'margin-left':'1024px'});  
+});
+$('#bloc_menu_3').on('webkitTransitionEnd', 
+function() {
+ $('#bloc_menu_4').css({'margin-left':'1024px'});  
+});
+
 });
 
 });
