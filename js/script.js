5089663e91f36b03e5ae3b246ca37ffb881389cd
nicopot <potin.nico@gmail.com>
2013-06-12 18:09:56 +0200

    conf index

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
 
 $('body').on('touchmove', function(evt) { evt.preventDefault(); })
 
-$('#page_start').swipe({swipeLeft:function(event, direction, distance, duration, fingerCount) {$(this).css({"-webkit-transform":"translateX(-2048px)"});$('#page_menu').show();$('#page_menu').css({"-webkit-transform":"translateX(-2048px)"});},threshold:0});
-$('#page_menu').swipe({swipeRight:function(event, direction, distance, duration, fingerCount) {$(this).css({"-webkit-transform":"translateX(+2048px)"});$('#page_start').css({"-webkit-transform":"translateX(0px)"});},threshold:0});
+$('#page_start').swipe({swipeLeft:function(event, direction, distance, duration, fingerCount) {$(this).css({"-webkit-transform":"translateX(-1024px)"});$('#page_menu').show();$('#page_menu').css({"-webkit-transform":"translateX(-1024px)"});},threshold:0});
+$('#page_menu').swipe({swipeRight:function(event, direction, distance, duration, fingerCount) {$(this).css({"-webkit-transform":"translateX(+1024px)"});$('#page_start').css({"-webkit-transform":"translateX(0px)"});},threshold:0});
 
 $('#page_menu').on('transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd', 
 function() {
