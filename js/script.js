adcf8228509b3a7f4c269d2e7e628038186db706
nicopot <potin.nico@gmail.com>
2013-06-17 13:58:47 +0200

    last change

--- js/script.js
+++ js/script.js
@@ -100,7 +100,7 @@ $('.bt_home_climaston_1').bind('touchstart',function(){$('#page_accueil_climasto
 $('.bt_home_climaston_2').bind('touchstart',function(){$('#page_accueil_climaston').transition({x:'0px'},function(){$('#page_climaston_1,#page_climaston_2').css({x:'0px'});});$('#page_menu').css({x:'-1024px'});});
 
 
-$('#bt_menu_1').bind('touchend',function(){
+$('#bt_menu_1').bind('touchstart',function(){
 
 	$('#page_menu').css({x:'-2048px'});
 	$('#page_accueil_duphaston').css({x:'-1024px'});
@@ -109,7 +109,7 @@ $('#bt_menu_1').bind('touchend',function(){
   $('#page_duphaston_bonusage').css('background-image','url(./images/eadv_decoupe_Page_06_light.jpg)'); 	
 });
 
-$('#bt_menu_2').bind('touchend',function(){
+$('#bt_menu_2').bind('touchstart',function(){
   $('#page_climaston_1').css('background-image','url(./images/eadv_decoupe_Page_08_light.jpg)');
   $('#page_climaston_2').css('background-image','url(./images/eadv_decoupe_Page_09_light.jpg)');
   $('#page_climaston_backup_1,#page_climaston_backup_2').css('background-image','url(./images/eadv_decoupe_Page_10_light.jpg)');
@@ -122,7 +122,7 @@ $('#bt_menu_2').bind('touchend',function(){
 	$('#page_accueil_climaston').css({x:'-1024px'});
 });
 
-$('#bt_menu_3').bind('touchend',function(){
+$('#bt_menu_3').bind('touchstart',function(){
 	$('#page_oesclim_1').css('background-image','url(./images/eadv_decoupe_Page_17_light.jpg)');
 	$('#page_oesclim_backup').css('background-image','url(./images/eadv_decoupe_Page_18_light.jpg)');
 	$('#page_oesclim_bonusage_1').css('background-image','url(./images/eadv_decoupe_Page_19_light.jpg)');
@@ -134,7 +134,7 @@ $('#bt_menu_3').bind('touchend',function(){
 	$('#page_accueil_oesclim').css({x:'-1024px'});
 });
 
-$('#bt_menu_4').bind('touchend',function(){
+$('#bt_menu_4').bind('touchstart',function(){
 	$('#page_oesclim_duphaston_1').css('background-image','url(./images/eadv_decoupe_Page_25_light.jpg)');
 	$('#page_oesclim_duphaston_2').css('background-image','url(./images/eadv_decoupe_Page_26_light.jpg)');
 	$('#page_menu').css({x:'-2048px'});
